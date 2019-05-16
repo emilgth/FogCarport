872caed552a38266516d3d91252f756a864c2452
Emil Hansen <emilgth@gmail.com>
2019-05-16 09:12:54 +0200

    BUGFIX: efter admin har bekræftet ordre vises tabellen med andre ordrer korrekt

--- src/main/java/PresentationLayer/ConfirmOrder.java
+++ src/main/java/PresentationLayer/ConfirmOrder.java
@@ -2,9 +2,11 @@ package PresentationLayer;
 
 import FunctionLayer.LogicFacade;
 import FunctionLayer.LoginSampleException;
+import FunctionLayer.Models.Order;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
 
 public class ConfirmOrder extends Command {
     @Override
@@ -12,6 +14,8 @@ public class ConfirmOrder extends Command {
         double newPrice = Double.parseDouble(request.getParameter("newPrice"));
         int orderId = Integer.parseInt(request.getParameter("orderId"));
         LogicFacade.confirmOrder(newPrice, orderId);
+        ArrayList<Order> orders = LogicFacade.getPendingOrders();
+        request.setAttribute("orders", orders);
         return "newOrders";
     }
 }
